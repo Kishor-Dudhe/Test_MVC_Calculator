6a3045f6f914ce35ad27eb846bb8d12931616a47
Kishor-Dudhe <kishor_dudhe@yahoo.com>
2015-03-04 16:37:04 +0530

    Unit Test on MVC Calculator

---
+++ Test_MVC_Calculator/Test_MVC_Calculator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MVC_Calc;
+
+namespace Test_MVC_Calculator
+{
+    [TestClass]
+    public class Test_MVC_Calculator
+    {
+        Calc_Controller c = new Calc_Controller();
+
+        [TestMethod]
+        public void MultiplicateTwoPositiveNumber()
+        {
+            int result;
+            result = c.mul(10, 5);
+            Assert.AreEqual(50m, result, "Multiplication of two positive number failed");
+        }
+
+        [TestMethod]
+        public void MultiplicationOnePositiveOneNegativeNumber()
+        {
+            int result;
+            result = c.mul(-10, 5);
+            Assert.AreEqual(-50m, result, "Multiplication of one positive one negative number failed");
+        }
+
+        [TestMethod]
+        public void MultiplicateTwoNegativeNumber()
+        {
+            int result;
+            result = c.mul(-10, -5);
+            Assert.AreEqual(50m, result, "Multiplication of two neagtive number failed");
+        }
+
+        [TestMethod]
+        public void MultiplicateOneNumberOneCharacter()
+        {
+            int result;
+            result = c.mul('a', 5);
+            Assert.AreEqual(485m, result, "Multiplication of one Number and one character failed");
+        }
+    }
+}
